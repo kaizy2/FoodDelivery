9b35ae8ef739c983fecddae188cbe0aa0c41245c
tutien2904 <tutien29042003@gmail.com>
2025-03-31 22:52:44 +0700

    add to favourite food

---
+++ Food/Pages/Users/Favorites.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Food.Repositories;
+using Models;
+using System.Threading.Tasks;
+
+namespace Food.Pages.Users
+{
+    public class FavoritesModel : PageModel
+    {
+        public void OnGet()
+        {
+            // Không cần xử lý logic vì dữ liệu lấy từ LocalStorage (client-side)
+        }
+    }
+}
